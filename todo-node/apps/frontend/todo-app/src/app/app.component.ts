e47cebcc2cc70e4e9ea279e1db589b9bd95750a0
m.kikmunter <M.Kikmunter@gmail.com>
2020-11-14 19:13:01 +0000

    initialized angular project

---
+++ todo-node/apps/frontend/todo-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'todo-node-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'frontend-todo-app';
+}
