0f9eb0e6f9e4f96f9cef61c8f17fa8889f86c2d8
m.kikmunter <M.Kikmunter@gmail.com>
2020-11-14 19:24:42 +0000

    installed nodemon

--- todo-node/apps/backend/todo-api/src/main.ts
+++ todo-node/apps/backend/todo-api/src/main.ts
@@ -11,7 +11,7 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to backend/todo-api!' });
 });
 
-const port = process.env.port || 3333;
+const port = process.env.port || 4000;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
