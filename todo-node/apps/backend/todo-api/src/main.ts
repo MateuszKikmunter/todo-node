5185c0430792a0572b14b4c1ae2f195228653245
m.kikmunter <M.Kikmunter@gmail.com>
2020-11-28 19:04:22 +0000

    main.ts cleanup

--- todo-node/apps/backend/todo-api/src/main.ts
+++ todo-node/apps/backend/todo-api/src/main.ts
@@ -1,34 +1,17 @@
-import { Connection } from 'typeorm';
 import * as express from 'express';
 
-import { SqlConnectionFactory, User } from '@todo-node/server/database';
+const app = express();
 
-//TODO: TEMPORARY DB CONNECTION TEST => TO REMOVE LATER
-SqlConnectionFactory.createConnection('sqlite').then((connection: Connection) => {
+app.get('/api', async (req, res) => {
+    try {
+        res.json({ message: 'Welcome to the api!' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error: error.message });
+    }
+});
 
-    const app = express();
-    const repo = connection.getRepository(User);
-
-    app.get('/api', async (req, res) => {
-        try {
-            const users = await repo.find();
-    
-            res.json(users ?? { message: 'No data found!' });
-        } catch (error) {
-            console.log(error);
-            res.status(500).send({ error: error.message });
-        }
-    });
-
-    const port = process.env.port || 4000;
-    const server = app.listen(port, () => {
-        console.log(`Listening at http://localhost:${port}/api`);
-    });
-    server.on('error', console.error);
-    server.on('close', connection.close);
-}).catch(error => console.log(error));
-
-const start = async () => {
-    const dbConnection = await SqlConnectionFactory.createConnection('sqlite');
-    dbConnection;
-};
+const port = process.env.port || 4000;
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/api`);
+});
