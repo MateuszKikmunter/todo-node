ecc4143f79cf62a4c9547af8d6050dee86f5f57f
m.kikmunter <M.Kikmunter@gmail.com>
2020-12-08 21:17:48 +0000

    added shared lib

--- todo-node/jest.config.js
+++ todo-node/jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  projects: [
-    '<rootDir>/apps/frontend/todo-app',
-    '<rootDir>/apps/backend/todo-api',
-    '<rootDir>/libs/todo-web',
-    '<rootDir>/libs/shared',
-    '<rootDir>/libs/server/database',
-  ],
+    projects: [
+        '<rootDir>/apps/frontend/todo-app',
+        '<rootDir>/apps/backend/todo-api',
+        '<rootDir>/libs/todo-web',
+        '<rootDir>/libs/shared',
+        '<rootDir>/libs/server/database',
+        '<rootDir>/libs/shared/utils',
+    ],
 };
---
+++ todo-node/libs/shared/utils/src/lib/enums/http.code.ts
@@ -0,0 +1,9 @@
+export enum HttpCode {
+    CREATED = 201,
+    OK = 200,
+    NO_CONTENT = 204,
+    BAD_REQUEST = 400,
+    NOT_FOUND = 404,
+    UNPROCESSABLE_ENTITY = 422,
+    SERVER_ERROR = 500
+}
