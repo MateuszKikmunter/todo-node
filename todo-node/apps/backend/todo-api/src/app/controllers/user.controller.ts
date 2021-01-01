4face8ef19ecad258977a42e06d2cb5dd810d009
m.kikmunter <M.Kikmunter@gmail.com>
2021-01-01 15:57:24 +0000

    returning simplfied users

--- todo-node/apps/backend/todo-api/src/app/controllers/user.controller.ts
+++ todo-node/apps/backend/todo-api/src/app/controllers/user.controller.ts
@@ -16,7 +16,13 @@ export class UserController {
     public async getAll(req: Request, res: Response): Promise<Response> {
         try {
             const users: User[] = await getConnection('sqlite').getRepository(User).find();
-            return res.status(HttpCode.OK).json(users);
+            return res.status(HttpCode.OK).json(users?.map(user => {
+                return { 
+                    id: user.id,
+                    email: user.email,
+                    todos: user?.todos
+                };
+            }));
         } catch (err) {
             console.log(err);
             return res.status(HttpCode.BAD_REQUEST).json({ error: 'Something went wrong!' });
