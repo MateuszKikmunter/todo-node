0a13a6153e0ab7cfdbad802772b0f6ffb4a526eb
m.kikmunter <M.Kikmunter@gmail.com>
2021-01-02 20:04:10 +0000

    added 401 to the codes

--- todo-node/libs/shared/utils/src/lib/enums/http.code.ts
+++ todo-node/libs/shared/utils/src/lib/enums/http.code.ts
@@ -3,6 +3,7 @@ export enum HttpCode {
     OK = 200,
     NO_CONTENT = 204,
     BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
     NOT_FOUND = 404,
     UNPROCESSABLE_ENTITY = 422,
     SERVER_ERROR = 500
