94d0760027de19d77e187a5af4280f3ad1cf09de
m.kikmunter <M.Kikmunter@gmail.com>
2021-01-09 21:42:18 +0000

    fixed duped properties from declaration merging

--- todo-node/apps/backend/todo-api/src/app/shared/types/custom.d.ts
+++ todo-node/apps/backend/todo-api/src/app/shared/types/custom.d.ts
@@ -1,11 +1,10 @@
-//libs imports
-import { Request } from 'express-serve-static-core';
-
 //local imports
 import { CurrentUser } from './current.user';
 
-declare module 'express-serve-static-core' {
-    interface Request {
-        user?: CurrentUser;
+declare global {
+    namespace Express {
+        interface Request {
+            user?: CurrentUser;
+        }
     }
 }
