2b88c90070c75376db0343e6bba6fd042b5079ea
m.kikmunter <M.Kikmunter@gmail.com>
2021-01-14 22:29:22 +0000

    added 403

--- todo-node/libs/shared/utils/src/lib/enums/http.code.ts
+++ todo-node/libs/shared/utils/src/lib/enums/http.code.ts
@@ -4,6 +4,7 @@ export enum HttpCode {
     NO_CONTENT = 204,
     BAD_REQUEST = 400,
     UNAUTHORIZED = 401,
+    FORBIDDEN = 403,
     NOT_FOUND = 404,
     UNPROCESSABLE_ENTITY = 422,
     SERVER_ERROR = 500
