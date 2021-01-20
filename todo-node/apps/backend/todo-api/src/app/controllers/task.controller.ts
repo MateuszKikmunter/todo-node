376aa0cc0d4cfbdcaac9c677c4df1330f695136c
m.kikmunter <M.Kikmunter@gmail.com>
2021-01-20 22:47:57 +0000

    POST task

--- todo-node/apps/backend/todo-api/src/app/controllers/task.controller.ts
+++ todo-node/apps/backend/todo-api/src/app/controllers/task.controller.ts
@@ -1,13 +1,30 @@
 //libs imports
-import { Task } from '@todo-node/server/database';
-import { getConnection, Repository } from 'typeorm';
+import { Task, User } from '@todo-node/server/database';
+import { HttpCode } from '@todo-node/shared/utils';
+import { Request, Response } from 'express';
+import { getConnection } from 'typeorm';
 
 
 export class TaskController {
 
-    private _taskRepository: Repository<Task>;
+    /**
+     * Creates task based on the payload and links it to the user sending the request.
+     * @param req request
+     * @param res response
+     */
+    public createTask = async (req: Request, res: Response): Promise<Response> => {
+        try {
+            const task = {         
+                ...req.body,
+                user: await getConnection('sqlite').getRepository(User).findOne({ id: req?.user.id })
+            } as Task;      
 
-    constructor() {
-        this._taskRepository = getConnection('sqlite').getRepository(Task);
+            await getConnection('sqlite').getRepository(Task).insert(task);
+            return res.sendStatus(HttpCode.OK);
+            
+        } catch (err) {
+            console.log(err);
+            return res.status(HttpCode.BAD_REQUEST).json({ error: err.message });
+        }    
     }
 }
--- todo-node/apps/backend/todo-api/src/app/routes/task.route.ts
+++ todo-node/apps/backend/todo-api/src/app/routes/task.route.ts
@@ -3,13 +3,16 @@ import { Router } from 'express';
 
 //local imports
 import { Route } from '../shared/interfaces/route';
+import { taskValidationRules, validateTaskAddOrUpdateRequest } from '../shared/validators/task.validator';
 import { TaskController } from './../controllers/task.controller';
+import { JwtMiddleware } from './../middleware/jwt.middleware';
 
 
 export class TaskRoute implements Route {
 
     private _url: string = "/api/task";
     private _taskController: TaskController;
+    private _jwtMiddleware: JwtMiddleware;
     private _router: Router;
 
     get router(): Router {
@@ -19,11 +22,15 @@ export class TaskRoute implements Route {
     constructor() {
         this._router = Router();
         this._taskController = new TaskController();
+        this._jwtMiddleware = new JwtMiddleware();
         this.initRoutes();
     }
     
     /** Initialize routes handled by this route. */
     private initRoutes(): void {
-        //TODO: init routes here (using methods exposed by the task controller )
+        this._router.post(`${ this._url }/create`, this._jwtMiddleware.validateJwtToken, taskValidationRules, [ 
+            validateTaskAddOrUpdateRequest, 
+            this._taskController.createTask 
+        ]);        
     }
 }
