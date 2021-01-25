511ebfdd3f5002f6c0893d95cd7316cca118134c
m.kikmunter <M.Kikmunter@gmail.com>
2021-01-25 20:56:37 +0000

    export footer cmpnt

--- todo-node/libs/todo-app/shared/ui-footer/src/lib/todo-app-shared-ui-footer.module.ts
+++ todo-node/libs/todo-app/shared/ui-footer/src/lib/todo-app-shared-ui-footer.module.ts
@@ -1,10 +1,14 @@
 //Angular imports
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+//local imports
 import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
     imports: [CommonModule],
-    declarations: [FooterComponent]
+    declarations: [FooterComponent],
+    exports: [FooterComponent]
 })
 export class TodoAppSharedUiFooterModule {}
