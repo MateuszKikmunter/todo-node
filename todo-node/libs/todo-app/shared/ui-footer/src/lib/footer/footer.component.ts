ae3210cd91d3eee9600b4afa1e818ba9bf4196cb
m.kikmunter <M.Kikmunter@gmail.com>
2021-02-14 16:21:21 +0000

    basic app layout structure done

--- todo-node/libs/todo-app/shared/ui-footer/src/lib/footer/footer.component.ts
+++ todo-node/libs/todo-app/shared/ui-footer/src/lib/footer/footer.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'todo-node-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FooterComponent {
-
   readonly currentYear = new Date().getFullYear();
 }
--- todo-node/libs/todo-app/shared/ui-header/src/lib/todo-app-shared-ui-header.module.ts
+++ todo-node/libs/todo-app/shared/ui-header/src/lib/todo-app-shared-ui-header.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TodoAppSharedUtilModule } from '@todo-node/todo-app/shared/util';
+
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [
+        CommonModule, 
+        TodoAppSharedUtilModule
+    ],
     declarations: [HeaderComponent],
     exports: [HeaderComponent]
 })
