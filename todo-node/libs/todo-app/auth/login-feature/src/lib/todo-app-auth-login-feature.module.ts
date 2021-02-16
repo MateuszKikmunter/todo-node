3467625129e69af31b2c0c498247cb20c545b63e
m.kikmunter <M.Kikmunter@gmail.com>
2021-02-16 17:33:33 +0000

    added LoginFormComponent

--- todo-node/libs/todo-app/auth/login-feature/src/lib/todo-app-auth-login-feature.module.ts
+++ todo-node/libs/todo-app/auth/login-feature/src/lib/todo-app-auth-login-feature.module.ts
@@ -2,16 +2,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//libs imports
+import { PasswordModule } from 'primeng/password';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { TodoAppSharedUiLayoutModule } from '@todo-node/todo-app/shared/ui-layout';
+import { ButtonModule } from 'primeng/button';
+
 //local imports
 import { LoginComponent } from './login/login.component';
 import { TodoAppAuthLoginRoutingModule } from './todo-app-auth-login-routing.module';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 
 @NgModule({
     imports: [
         CommonModule,
+        InputTextModule,
+        TodoAppSharedUiLayoutModule,
+        PasswordModule,
+        CardModule,
+        ButtonModule,
         TodoAppAuthLoginRoutingModule
     ],
-    declarations: [LoginComponent]
+    declarations: [
+        LoginComponent, 
+        LoginFormComponent
+    ]
 })
 export class TodoAppAuthLoginFeatureModule {}
