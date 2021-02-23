4e97e90c77daa1eb84cf32b5e5d19a8388882cfd
m.kikmunter <M.Kikmunter@gmail.com>
2021-02-23 00:30:58 +0000

    token interceptor init

--- todo-node/libs/todo-app/auth/data-access/src/index.ts
+++ todo-node/libs/todo-app/auth/data-access/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/todo-app-auth-data-access.module';
 export * from './lib/+state/auth-facade.service';
+export * from './lib/interceptors/token.interceptor';
--- todo-node/libs/todo-app/auth/data-access/src/lib/todo-app-auth-data-access.module.ts
+++ todo-node/libs/todo-app/auth/data-access/src/lib/todo-app-auth-data-access.module.ts
@@ -1,13 +1,23 @@
 //Angular imports
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+//local imports
+import { TokenInterceptor } from './interceptors/token.interceptor';
 
 
 @NgModule({
     imports: [
         CommonModule,
         HttpClientModule
+    ],
+    providers: [
+        { 
+            provide: HTTP_INTERCEPTORS, 
+            useClass: TokenInterceptor, 
+            multi: true 
+        }
     ]
 })
 export class TodoAppAuthDataAccessModule {}
