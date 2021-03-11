ab2c6217308771497c6e52e7787ad2adfaf6467c
m.kikmunter <M.Kikmunter@gmail.com>
2021-03-11 01:29:32 +0000

    data load fix

--- todo-node/libs/todo-app/todo/data-access/src/lib/services/todo.store.ts
+++ todo-node/libs/todo-app/todo/data-access/src/lib/services/todo.store.ts
@@ -1,10 +1,10 @@
-import { BehaviorSubject } from 'rxjs';
 //Angular imports
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 //libs imports
 import { Observable } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 import { switchMap, withLatestFrom } from 'rxjs/operators';
 import { CurrentUser } from '@todo-node/shared/utils';
 import { AuthFacadeService } from '@todo-node/todo-app/auth/data-access';
@@ -16,7 +16,8 @@ import { Task } from '@todo-node/shared/utils';
 export class TodoStore {
 
   private readonly taskApiUrl = 'http://localhost:4000/api/task';
-  private _tasks: BehaviorSubject<Task[]> = new BehaviorSubject<Task[]>(null);
+
+  private _tasks: BehaviorSubject<Task[]> = new BehaviorSubject<Task[]>([]);
   public readonly tasks$: Observable<Task[]> = this._tasks.asObservable();
 
   constructor(private http: HttpClient,
@@ -26,10 +27,12 @@ export class TodoStore {
     }
 
     /** Gets currently logged in user tasks via HTTP and saves the result in the store. */
-  private getUserTasks(): void {
+  private getUserTasks(): void {    
     this.authFacade.getCurrentUser().pipe(
       withLatestFrom((user: CurrentUser) => user?.id),
-      switchMap((userID: string) => this.http.get<Task[]>(`${this.taskApiUrl}/user/${userID}`))
+      switchMap((userID: string) => {
+        return userID ? this.http.get<Task[]>(`${this.taskApiUrl}/user/${userID}`) : of([])
+      })
     ).subscribe((tasks: Task[]) => this._tasks.next(tasks));
     
   }
