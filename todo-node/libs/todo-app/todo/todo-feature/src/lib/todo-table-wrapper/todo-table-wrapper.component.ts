aab8c7df30aad1799986f40e28282ef0b73ebf4f
m.kikmunter <M.Kikmunter@gmail.com>
2021-03-11 01:29:44 +0000

    renamed selectors

--- todo-node/libs/todo-app/todo/todo-feature/src/lib/todo-table-wrapper/todo-table-wrapper.component.ts
+++ todo-node/libs/todo-app/todo/todo-feature/src/lib/todo-table-wrapper/todo-table-wrapper.component.ts
@@ -6,8 +6,9 @@ import { Observable } from 'rxjs';
 import { Task } from '@todo-node/shared/utils';
 import { TodoFacadeService } from '@todo-node/todo-app/todo/data-access';
 
+
 @Component({
-  selector: 'todo-node-todo-table-wrapper',
+  selector: 'todo-table-wrapper',
   templateUrl: './todo-table-wrapper.component.html',
   styleUrls: ['./todo-table-wrapper.component.scss']
 })
