18a277cb62240d53f4ddc91f952c19fb47d50dc9
m.kikmunter <M.Kikmunter@gmail.com>
2021-03-18 00:06:55 +0000

    handle task selection in store

--- todo-node/libs/todo-app/todo/data-access/src/lib/services/todo-facade.service.ts
+++ todo-node/libs/todo-app/todo/data-access/src/lib/services/todo-facade.service.ts
@@ -2,7 +2,6 @@
 import { Injectable } from '@angular/core';
 
 //libs imports
-import { Observable } from 'rxjs';
 import { Task } from '@todo-node/shared/utils';
 
 //local imports
@@ -10,13 +9,30 @@ import { TodoStore } from './todo.store';
 
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class TodoFacadeService {
 
-  constructor(private store: TodoStore) { }
+    constructor(private store: TodoStore) {}
 
-  public getUserTasks(): Observable<Task[]> {
-    return this.store.tasks$;
-  }
+    /** Returns tasks for currently logged in user. */
+    readonly userTasks$ = this.store.tasks$;
+
+    /** Returns currently selected task. */
+    readonly selectedTask$ = this.store.selectedTask$;
+
+    /** Sets/clears currently selected task in the store. */
+    public selectTask(task: Task): void {
+        this.store.selectTask(task);
+    }
+
+    //TODO: to implent
+    public deleteTask(taskId: string): void {
+
+    }
+
+    //TODO: to implement
+    public createTask(task: Task): void {
+
+    }
 }
--- todo-node/libs/todo-app/todo/data-access/src/lib/services/todo.store.ts
+++ todo-node/libs/todo-app/todo/data-access/src/lib/services/todo.store.ts
@@ -10,6 +10,7 @@ import { CurrentUser } from '@todo-node/shared/utils';
 import { AuthFacadeService } from '@todo-node/todo-app/auth/data-access';
 import { Task } from '@todo-node/shared/utils';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -20,10 +21,18 @@ export class TodoStore {
   private _tasks: BehaviorSubject<Task[]> = new BehaviorSubject<Task[]>([]);
   public readonly tasks$: Observable<Task[]> = this._tasks.asObservable();
 
+  private _selectedTask: BehaviorSubject<Task> = new BehaviorSubject<Task>(null);
+  public readonly selectedTask$: Observable<Task> = this._selectedTask.asObservable();
+
   constructor(private http: HttpClient,
     private authFacade: AuthFacadeService) {
       //load initial data on service creation 
       this.getUserTasks();
+    }
+
+    /** Sets currently selected task in the store. */
+    public selectTask(task: Task): void {     
+      this._selectedTask.next(task ? { ...task } : null);   
     }
 
     /** Gets currently logged in user tasks via HTTP and saves the result in the store. */
