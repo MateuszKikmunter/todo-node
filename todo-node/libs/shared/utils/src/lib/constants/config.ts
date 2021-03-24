5d94da4d972cf7a9ba9c2516ed6fe9277cd53bff
m.kikmunter <M.Kikmunter@gmail.com>
2021-03-24 23:31:31 +0000

    fixed date format description

--- todo-node/libs/shared/utils/src/lib/constants/config.ts
+++ todo-node/libs/shared/utils/src/lib/constants/config.ts
@@ -1,5 +1,5 @@
 /** Standard date format in uppercase case - DD/MM/YYYY */
 export const DD_MM_YYYY = 'DD/MM/YYYY';
 
-/** Standard date format with time - dd/MM/yyyy */
+/** Standard date format - dd/MM/yyyy */
 export const dd_MM_yyyy = 'dd/MM/yyyy';
