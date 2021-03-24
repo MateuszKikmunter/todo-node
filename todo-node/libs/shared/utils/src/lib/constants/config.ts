ee11da55beca21710352d2b4bc8edb62ce3c718f
m.kikmunter <M.Kikmunter@gmail.com>
2021-03-24 23:27:26 +0000

    added dd/MM/yyyy

--- todo-node/libs/shared/utils/src/lib/constants/config.ts
+++ todo-node/libs/shared/utils/src/lib/constants/config.ts
@@ -1,2 +1,5 @@
 /** Standard date format in uppercase case - DD/MM/YYYY */
 export const DD_MM_YYYY = 'DD/MM/YYYY';
+
+/** Standard date format with time - dd/MM/yyyy */
+export const dd_MM_yyyy = 'dd/MM/yyyy';
