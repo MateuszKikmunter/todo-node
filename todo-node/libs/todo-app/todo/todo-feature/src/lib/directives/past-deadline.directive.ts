06fecf48e04f3087359de41564e30c7fa277e3ea
m.kikmunter <M.Kikmunter@gmail.com>
2021-03-26 23:01:49 +0000

    using pipe + moved logic to service in directive

--- todo-node/libs/todo-app/todo/todo-feature/src/lib/directives/past-deadline.directive.ts
+++ todo-node/libs/todo-app/todo/todo-feature/src/lib/directives/past-deadline.directive.ts
@@ -1,10 +1,8 @@
 //Angular imports
 import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 
-//libs imports
-import { DD_MM_YYYY } from '@todo-node/shared/utils';
-import * as dayjs from 'dayjs';
-import * as customParseFormat from 'dayjs/plugin/customParseFormat';
+//local imports
+import { DeadlineValidatorService } from './../services/deadline-validator.service';
 
 
 @Directive({
@@ -16,21 +14,13 @@ export class PastDeadlineDirective implements OnInit {
 
   constructor(
     private elementRef: ElementRef,
-    private renderer: Renderer2) { }
+    private renderer: Renderer2,
+    private deadlineValidator: DeadlineValidatorService) { }
 
   ngOnInit(): void {
-    if(this.isDeadlineMissed()) {      
-      this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'orange');
+    if(this.deadlineValidator.isDeadlineMissed(this.deadline)) {
+      this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'orange');      
     }
-  }
-
-  /** 
-   * * Checks if deadline has been missed.
-   * * Deadline is missed if it's before today's date.
-   */
-  private isDeadlineMissed(): boolean {
-    dayjs.extend(customParseFormat);
-    return dayjs().isAfter(dayjs(this.deadline, DD_MM_YYYY));
   }
 
 }
