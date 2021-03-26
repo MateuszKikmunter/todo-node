707f775a308460013a1413b49f88087f3d0b1dcc
m.kikmunter <M.Kikmunter@gmail.com>
2021-03-26 17:07:12 +0000

    main functionality works, styling change to update

--- todo-node/libs/todo-app/todo/todo-feature/src/lib/directives/past-deadline.directive.ts
+++ todo-node/libs/todo-app/todo/todo-feature/src/lib/directives/past-deadline.directive.ts
@@ -1,13 +1,34 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+//Angular imports
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+
+//libs imports
+import { DD_MM_YYYY } from '@todo-node/shared/utils';
+import * as dayjs from 'dayjs';
+import * as customParseFormat from 'dayjs/plugin/customParseFormat';
+
 
 @Directive({
   selector: '[taskPastDeadline]'
 })
 export class PastDeadlineDirective implements OnInit {
 
+  @Input('taskPastDeadline') deadline: string;
+
   constructor(private elementRef: ElementRef) { }
 
   ngOnInit(): void {
+    if(this.isDeadlineMissed()) {
+      this.elementRef.nativeElement.style.color = 'orange';
+    }
+  }
+
+  /** 
+   * * Checks if deadline has been missed.
+   * * Deadline is missed if it's before today's date.
+   */
+  private isDeadlineMissed(): boolean {
+    dayjs.extend(customParseFormat);
+    return dayjs().isAfter(dayjs(this.deadline, DD_MM_YYYY));
   }
 
 }
