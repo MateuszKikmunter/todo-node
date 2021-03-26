6f6bfb0118aaf64941c4381d04a93eb463e9e080
m.kikmunter <M.Kikmunter@gmail.com>
2021-03-26 22:28:09 +0000

    using Rendere2 to manipulate element style

--- todo-node/libs/todo-app/todo/todo-feature/src/lib/directives/past-deadline.directive.ts
+++ todo-node/libs/todo-app/todo/todo-feature/src/lib/directives/past-deadline.directive.ts
@@ -1,5 +1,5 @@
 //Angular imports
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 
 //libs imports
 import { DD_MM_YYYY } from '@todo-node/shared/utils';
@@ -14,11 +14,13 @@ export class PastDeadlineDirective implements OnInit {
 
   @Input('taskPastDeadline') deadline: string;
 
-  constructor(private elementRef: ElementRef) { }
+  constructor(
+    private elementRef: ElementRef,
+    private renderer: Renderer2) { }
 
   ngOnInit(): void {
-    if(this.isDeadlineMissed()) {
-      this.elementRef.nativeElement.style.color = 'orange';
+    if(this.isDeadlineMissed()) {      
+      this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'orange');
     }
   }
 
