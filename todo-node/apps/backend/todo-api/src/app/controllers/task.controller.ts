b193094a4a63ac73f387639d3914e472037490a5
m.kikmunter <M.Kikmunter@gmail.com>
2021-04-11 22:45:46 +0100

    TODO: note to self

--- todo-node/apps/backend/todo-api/src/app/controllers/task.controller.ts
+++ todo-node/apps/backend/todo-api/src/app/controllers/task.controller.ts
@@ -106,8 +106,9 @@ export class TaskController {
      * @param res response
      */
     public getUserTasks = async (req: Request, res: Response): Promise<Response> => {
-        //TODO: fix ordering by completed
-        //TODO: fix ordering by date
+        //TODO: fix sorting by completed
+        //TODO: fix sorting by date
+        //TODO: fix sorting when numeric names
         try {
             let query = getConnection('sqlite')
                 .getRepository(Task)
