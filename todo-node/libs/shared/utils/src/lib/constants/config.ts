fb6685ed62c62a0634c64814602aae1c50a13333
m.kikmunter <M.Kikmunter@gmail.com>
2021-04-14 17:22:13 +0100

    removed not used imports

--- todo-node/libs/shared/utils/src/lib/constants/config.ts
+++ todo-node/libs/shared/utils/src/lib/constants/config.ts
@@ -1,6 +1,3 @@
-import { SortOrder } from '../enums/sort.order';
-import { TaskRequestPayload } from './../interfaces/task-request.payload';
-
 
 /** Standard date format in uppercase case - DD/MM/YYYY */
 export const DD_MM_YYYY = 'DD/MM/YYYY';
