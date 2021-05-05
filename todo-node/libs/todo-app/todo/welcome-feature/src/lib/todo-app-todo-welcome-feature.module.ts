3530042823e344ffac3ed432d5beaf200236e425
m.kikmunter <M.Kikmunter@gmail.com>
2021-05-05 11:42:15 +0100

    (feat) welcome initial draft done

--- todo-node/libs/todo-app/todo/welcome-feature/src/lib/todo-app-todo-welcome-feature.module.ts
+++ todo-node/libs/todo-app/todo/welcome-feature/src/lib/todo-app-todo-welcome-feature.module.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 //libs imports
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { TodoAppSharedUiLayoutModule } from '@todo-node/todo-app/shared/ui-layout';
 
 //local imports
 import { WelcomeComponent } from './welcome/welcome.component';
@@ -12,11 +13,13 @@ import { TodoAppWelcomeRoutingModule } from './todo-app-welcome-feature-routing.
 
 
 @NgModule({
-    imports: [      
-      ButtonModule, 
-      CardModule,
-      CommonModule,
-      TodoAppWelcomeRoutingModule],
-    declarations: [WelcomeComponent]    
+    imports: [
+        ButtonModule,
+        CardModule,
+        CommonModule,
+        TodoAppSharedUiLayoutModule,
+        TodoAppWelcomeRoutingModule
+    ],
+    declarations: [WelcomeComponent]
 })
 export class TodoAppTodoWelcomeFeatureModule {}
