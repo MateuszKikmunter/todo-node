1672e1c5401b87e8e69e9ad2e2c7390ffe531cc1
m.kikmunter <M.Kikmunter@gmail.com>
2021-05-05 16:36:00 +0100

    (feat) about cmpnt init

--- todo-node/libs/todo-app/todo/about-feature/src/lib/todo-app-todo-about-feature.module.ts
+++ todo-node/libs/todo-app/todo/about-feature/src/lib/todo-app-todo-about-feature.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
     imports: [CommonModule],
+    declarations: [
+      AboutComponent
+    ],
 })
 export class TodoAppTodoAboutFeatureModule {}
